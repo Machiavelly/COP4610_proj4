5838a8d95d20e51c3326720dbda61023eb849629
Carlos Vera <c.v004@hotmail.com>
2018-12-02 02:08:22 -0500

    Add FIFO support in memMan, especially using function int findVictimPage(). Still need to call this in the appropriate places

--- nachos-3.4/code/userprog/memMan.cc
+++ nachos-3.4/code/userprog/memMan.cc
@@ -2,18 +2,27 @@
 
 memMan::memMan() {
     int i;
-    for (i = 0; i < 32; i++) {
+    for (i = 0; i < NUM_PAGES; i++) {
         mem[i] = false;
+        orderAllocated[i] = -1;
     }
+    orderAllocatedCounter = 0;
+    nextToEvict = 0; //requires the first page to be allocated to always be at index 0
 }
 
 int memMan::allocate() {
     int i;
-    for (i = 0; i < 32; i++) {
+    for (i = 0; i < NUM_PAGES; i++) {
         if (mem[i] == false) {
             mem[i] = true;
             numPages++;
             pageIndex++;
+            
+            //update the circular counter 
+            orderAllocatedCounter++;
+            orderAllocatedCounter %= NUM_PAGES;
+            
+            orderAllocated[i] = orderAllocatedCounter;
             return i;
         }
     }
@@ -24,8 +33,20 @@ void memMan::deallocate(int pageNum) {
     mem[pageNum] = false;
     numPages--;
     pageIndex--;
+    nextToEvict++;
 }
 
 int memMan::getPages() {
     return (NumPhysPages - numPages);
+}
+
+int memMan::findVictimPage() {
+    int i;
+    for (i = 0; i < NUM_PAGES; i++) {
+        if (orderAllocated[i] == nextToEvict) {
+            ASSERT(mem[i] == true); //make sure the next page to evict has been allocated previously
+            return i;
+        }
+    }
+    return -1;
 }
--- nachos-3.4/code/userprog/memMan.h
+++ nachos-3.4/code/userprog/memMan.h
@@ -1,4 +1,5 @@
 #include "machine.h"
+#define NUM_PAGES 32
 
 class memMan {
 public:
@@ -7,10 +8,16 @@ public:
     int allocate();
     void deallocate(int pageNum);
     int getPages();
+    //page swapping
+    int findVictimPage();
 
 private:
-    bool mem[32];
+    bool mem[NUM_PAGES];
     int numPages;
     int pageIndex;
+    //page swapping
+    int orderAllocated[NUM_PAGES];
+    int orderAllocatedCounter; //circular counter from 0 to [NUM_PAGES - 1]
+    int nextToEvict;
 
 };
